e8614fd5d1339e36a2698a5413122a88d7ac7304
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-03-31 07:59:53 +0200

    Create a class Employee

---
+++ src/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+    String firstName;
+    String lastName;
+    double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Imię: " + firstName + " Nazwisko: " + lastName + " WYpłata: " + salary;
+    }
+}

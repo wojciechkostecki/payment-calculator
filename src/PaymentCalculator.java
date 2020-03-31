9a1370197fb17d56ac7ab4c44edca90261d749ce
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-03-31 08:11:54 +0200

    Adding methods to calculate the annual net and gross payment

--- src/PaymentCalculator.java
+++ src/PaymentCalculator.java
@@ -1,3 +1,10 @@
 public class PaymentCalculator {
+    private static double nettoYearPayment(Employee employee) {
+        return employee.salary * 12;
+    }
 
+    private static double bruttoYearPayment(Employee employee) {
+        double zus = 1000;
+        return employee.salary * 12 + (zus * 12) + (employee.salary * 0.2 * 12);
+    }
 }

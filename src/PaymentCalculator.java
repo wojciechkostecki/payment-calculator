2131b76d98e73b2fb76878e091398e51ce76885b
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-05 20:50:26 +0200

    Improved final field naming

--- src/PaymentCalculator.java
+++ src/PaymentCalculator.java
@@ -1,12 +1,12 @@
 public class PaymentCalculator {
-    final double zus = 1000;
-    final double tax = 0.2;
+    private final static double ZUS = 1000;
+    private final static double TAX = 0.2;
 
     public double nettoYearPayment(Employee employee) {
         return employee.salary * 12;
     }
 
     public double bruttoYearPayment(Employee employee) {
-        return employee.salary * 12 + (zus * 12) + (employee.salary * tax * 12);
+        return employee.salary * 12 + (ZUS * 12) + (employee.salary * TAX * 12);
     }
 }

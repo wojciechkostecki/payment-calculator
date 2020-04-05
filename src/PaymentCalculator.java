38f2059bbf077d9fed318fed51bb86f310a4eaee
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-05 11:06:19 +0200

    Creation of final fields

--- src/PaymentCalculator.java
+++ src/PaymentCalculator.java
@@ -1,11 +1,12 @@
 public class PaymentCalculator {
+    final double zus = 1000;
+    final double tax = 0.2;
 
     public double nettoYearPayment(Employee employee) {
         return employee.salary * 12;
     }
 
     public double bruttoYearPayment(Employee employee) {
-        double zus = 1000;
-        return employee.salary * 12 + (zus * 12) + (employee.salary * 0.2 * 12);
+        return employee.salary * 12 + (zus * 12) + (employee.salary * tax * 12);
     }
 }
